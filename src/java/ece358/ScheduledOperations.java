56fc6ce6195ff738486e7d351fdf0807844048dd
Josh Halmo <josh.halmo@gmail.com>
2014-03-23 14:31:28 -0400

    Added ScheduledOperations functionality for Doctors and Staff

--- src/java/ece358/ScheduledOperations.java
+++ src/java/ece358/ScheduledOperations.java
@@ -45,6 +45,7 @@ public class ScheduledOperations extends HttpServlet {
             Users user = (Users) request.getSession().getAttribute("user");
             String role = user.getRole();
             String queryString = new String();
+            boolean addPatientInfo = false;
 
             if(role.equals(Constants.PATIENT))
             {
@@ -53,13 +54,22 @@ public class ScheduledOperations extends HttpServlet {
                        "WHERE SO.VisitID=V.VisitID && S.UserID=SO.DoctorID && O.Name=SO.OperationName && V.Timestamp= "+
                        "(SELECT max(Timestamp) FROM visitation WHERE VisitID = V.VisitID) && V.PatientID='" + user.getUserId() + "'";
             }
-            else if(role.equals(Constants.PATIENT))
+            else if(role.equals(Constants.STAFF))
             {
-
+                String managingDoctorQuery = "(SELECT ManagingDoctorID FROM staff WHERE UserID='"+ user.getUserId() +"')";
+                 queryString = "SELECT SO.VisitID, SO.OperationDateTime, SO.OperationName, S.FirstName, S.LastName, O.Name, O.Description, O.EstTime, V.PatientID, P.FirstName, P.LastName, D.FirstName, D.LastName "+
+                       "FROM scheduledoperations as SO, visitation as V, staff as S, staff as D, operations as O, patients as P "+
+                       "WHERE SO.VisitID=V.VisitID && S.UserID=SO.DoctorID && O.Name=SO.OperationName && P.UserID = V.PatientID && V.Timestamp= "+
+                       "(SELECT max(Timestamp) FROM visitation WHERE VisitID = V.VisitID) && D.UserID=P.DefaultDoctorID && (P.DefaultDoctorID=" + managingDoctorQuery + " OR SO.DoctorID= "+ managingDoctorQuery +")";
+                addPatientInfo = true;
             }
             else if(role.equals(Constants.DOCTOR))
             {
-
+                queryString = "SELECT SO.VisitID, SO.OperationDateTime, SO.OperationName, S.FirstName, S.LastName, O.Name, O.Description, O.EstTime, V.PatientID, P.FirstName, P.LastName, D.FirstName, D.LastName "+
+                       "FROM scheduledoperations as SO, visitation as V, staff as S, staff as D, operations as O, patients as P "+
+                       "WHERE SO.VisitID=V.VisitID && S.UserID=SO.DoctorID && O.Name=SO.OperationName && P.UserID = V.PatientID && V.Timestamp= "+
+                       "(SELECT max(Timestamp) FROM visitation WHERE VisitID = V.VisitID) && D.UserID=P.DefaultDoctorID && (P.DefaultDoctorID='" + user.getUserId() + "' OR SO.DoctorID='"+ user.getUserId() +"')";
+                addPatientInfo = true;
             }
             List<Object[]> result = SQLSessionUtil.executeQuery(queryString);
 
@@ -67,9 +77,13 @@ public class ScheduledOperations extends HttpServlet {
             List<Scheduledoperations> schedoperationsPast = new ArrayList<Scheduledoperations>();
             List<Operations> operationsPast = new ArrayList<Operations>();
             List<Staff> surgeonsPast = new ArrayList<Staff>();
+            List<Staff> doctorsPast = new ArrayList<Staff>();
+            List<Patients> patientsPast = new ArrayList<Patients>();
             List<Scheduledoperations> schedoperationsFuture = new ArrayList<Scheduledoperations>();
             List<Operations> operationsFuture = new ArrayList<Operations>();
             List<Staff> surgeonsFuture = new ArrayList<Staff>();
+            List<Staff> doctorsFuture = new ArrayList<Staff>();
+            List<Patients> patientsFuture = new ArrayList<Patients>();
             Calendar cal = Calendar.getInstance();
             Date today = cal.getTime();
             for(Object[] o : result)
@@ -87,25 +101,54 @@ public class ScheduledOperations extends HttpServlet {
                 op.setName((String)o[5]);
                 op.setDescription((String)o[6]);
                 op.setEstTime((Time)o[7]);
+                Patients p = new Patients();
+                Staff d = new Staff();
+                if(addPatientInfo)
+                {
+                    p.setUserId((String)o[8]);
+                    p.setFirstName((String)o[9]);
+                    p.setLastName((String)o[10]);
+                    d.setFirstName((String)o[11]);
+                    d.setLastName((String)o[12]);
+                }
+                    
+
                 if(sop.getOperationDateTime().compareTo(today) <= 0)
                 {
                     surgeonsPast.add(st);
                     schedoperationsPast.add(sop);
                     operationsPast.add(op);
+                    if(addPatientInfo)
+                    {
+                        doctorsPast.add(d);
+                        patientsPast.add(p);
+                    }
+
                 }
                 else
                 {
                     surgeonsFuture.add(st);
                     schedoperationsFuture.add(sop);
                     operationsFuture.add(op);
+                    if(addPatientInfo)
+                    {
+                        doctorsFuture.add(d);
+                        patientsFuture.add(p);
+                    }
+                        
+                        
                 }
             }
             request.setAttribute("schedoperationsPast", schedoperationsPast);
             request.setAttribute("surgeonsPast", surgeonsPast);
+            request.setAttribute("doctorsPast", doctorsPast);
             request.setAttribute("operationsPast", operationsPast);
+            request.setAttribute("patientsPast", patientsPast);
             request.setAttribute("schedoperationsFuture", schedoperationsFuture);
             request.setAttribute("surgeonsFuture", surgeonsFuture);
+             request.setAttribute("doctorsFuture", doctorsFuture);
             request.setAttribute("operationsFuture", operationsFuture);
+            request.setAttribute("patientsFuture", patientsFuture);
             url = "/ScheduledOperations.jsp";
         }
         catch(Exception e)
