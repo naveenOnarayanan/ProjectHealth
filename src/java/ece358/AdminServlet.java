07ba033deb303a38fe89d83a70a9370ebfb2ebd0
Gordon <GordonZ1992@gmail.com>
2014-03-29 21:56:41 -0400

    Added barebones admin servlet

---
+++ src/java/ece358/AdminServlet.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ece358;
+
+import ece358.models.Users;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author GordonMBP
+ */
+public class AdminServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        Users sessionUser = (Users) request.getSession().getAttribute("user");
+        if (sessionUser == null) {
+            getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+            return;
+        }
+        
+        String url = "admin.jsp";
+        boolean queryServletError = false;
+        try {
+            
+            int mode = Integer.parseInt(request.getParameter("mode"));
+            
+            if (mode == 0)
+            {
+                
+            }
+            else if (mode == 1)
+            {
+            }
+            else 
+            {
+            }
+            
+            request.setAttribute("queryServletError", queryServletError);
+        } catch (Exception e) {
+            queryServletError = true;
+            request.setAttribute("error", e.getMessage());
+            request.setAttribute("queryServletError", queryServletError);
+            System.out.println(e);
+        }
+        getServletContext().getRequestDispatcher("/DoctorLookup.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
